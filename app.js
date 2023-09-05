3457b3f497c521d4819e702d7abc99b2437538fd
hanumansaini12511 <er.hanumansaini@gmail.com>
2023-09-05 11:57:08 +0530

    fix

--- app.js
+++ app.js
@@ -9,25 +9,20 @@ app.use(express.json())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors())
-
-app.use(express.static('public'));
-app.use('/src', express.static('src'));
-
 app.listen(3000,()=>{
   console.log("connect...");
 })
 
-const server =http.createServer(app)
 
+
+// ******** restapi start *********
 
 app.get('/', function(req,res){
-
     res.sendFile(path.join(__dirname,'./src/form/Form.html'));
 });
 
 app.post('/login', function(req,res){
   console.log(req.body);
-
- // res.send("data send")
-  res.sendFile(path.join(__dirname,'./src/Home/index.html'));
+  // res.send("data send")
+  res.sendFile(path.join(__dirname,'./src/Home/index.js'));
 });
